4697339bbe84aeefce9a3dbf41a57883e5b5ecaa
Rajput Vivek <167712861+Rajput-Vivek-16@users.noreply.github.com>
2024-10-17 00:45:44 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 // app.use(express.static('dist'));
 
