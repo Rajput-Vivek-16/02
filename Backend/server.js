648532b277e85a9b399a6b419cec15d94bc50e32
Rajput-Vivek-16 <rajputvivek9233@gmail.com>
2024-10-16 21:08:01 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,64 @@
+/* commonjs
+const express = require('express')
+require('dotenv').config()*/
+
+import express from 'express'//module.js   async
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+// app.use(express.static('dist'));
+
+const data = [
+    {
+      "id": 1,
+      "title": "Introduction to JavaScript",
+      "content": "JavaScript is a versatile programming language commonly used in web development."
+    },
+    {
+      "id": 2,
+      "title": "Getting Started with React",
+      "content": "React is a popular JavaScript library for building user interfaces."
+    },
+    {
+      "id": 3,
+      "title": "Understanding Node.js",
+      "content": "Node.js is a runtime environment that allows you to run JavaScript on the server."
+    },
+    {
+      "id": 4,
+      "title": "Introduction to HTML",
+      "content": "HTML is the standard markup language for creating web pages."
+    },
+    {
+      "id": 5,
+      "title": "Introduction to CSS",
+      "content": "CSS is used to style HTML elements and control the layout of web pages."
+    },
+    {
+      "id": 6,
+      "title": "What is Express.js?",
+      "content": "Express.js is a minimal and flexible Node.js web application framework."
+    },
+    {
+      "id": 7,
+      "title": "Understanding Git and GitHub",
+      "content": "Git is a version control system, and GitHub is a platform for hosting and collaborating on code repositories."
+    }
+  ]
+  
+// app.get('/', (req, res)=>{
+//     res.send("Server is ready");
+// })
+
+app.get('/api/info', (req, res)=>{
+    res.json(data);
+})
+
+
+app.listen(port, (req, res)=>{
+    console.log(`Serve at http://localhost:${port}`);
+})
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [infos, setInfos] = useState([])
+  useEffect(()=>{
+    axios.get('/api/info') //standard
+    .then((response) => {
+      setInfos(response.data)
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  })
+  return (
+    <>  
+      <h1>Connecting frontend and backend</h1>
+      <p>Infos: {infos.length}</p>
+        {infos.map((info)=>(
+          <div key={info.id}>
+            <h2>{info.title}</h2>
+            <p>{info.content}</p>
+          </div>
+        ))}
+    </>
+  )
+}
+
+export default App
